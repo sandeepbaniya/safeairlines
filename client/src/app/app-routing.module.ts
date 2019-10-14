759f0bdc42117da74965c9e1d276d2a637907338
iaidar <iaidar@mum.edu>
2019-10-14 11:31:02 -0500

    Add a basic template

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IndexComponent} from "./pages/index/index.component";
+import {ProfilepageComponent} from "./pages/examples/profilepage/profilepage.component";
+import {RegisterpageComponent} from "./pages/examples/registerpage/registerpage.component";
+import {LandingpageComponent} from "./pages/examples/landingpage/landingpage.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: IndexComponent },
+  { path: "profile", component: ProfilepageComponent },
+  { path: "register", component: RegisterpageComponent },
+  { path: "landing", component: LandingpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
