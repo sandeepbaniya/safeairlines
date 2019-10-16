3ddab25de390acbe0b0e953f71d6ccfb94e9b8bc
iaidar <iaidar@mum.edu>
2019-10-16 09:17:35 -0500

    Regestration controller

---
+++ server/safeairlines/src/main/java/edu/mum/cs/cs452/safeairlines/controller/RegistrationController.java
@@ -0,0 +1,51 @@
+package edu.mum.cs.cs452.safeairlines.controller;
+
+import edu.mum.cs.cs452.safeairlines.model.User;
+import edu.mum.cs.cs452.safeairlines.service.RoleService;
+import edu.mum.cs.cs452.safeairlines.service.UserService;
+import edu.mum.cs.cs452.safeairlines.utils.SecurityConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    @GetMapping("/registration")
+    public String registration(@ModelAttribute User user){
+        return "pages/security/registration";
+    }
+
+    @PostMapping("/registration")
+    public String saveUser(@Valid User user, BindingResult result, Model model){
+        User userExists = userService.findUserByUserName(user.getUsername());
+        if (userExists != null) {
+            result
+                    .rejectValue("username", "error.user",
+                            "There is already a user registered with the username provided");
+
+        }
+        if (result.hasErrors()) {
+            return "pages/security/registration";
+        }
+        user.setRole((roleService.getById(user.getRole().getId())));
+        userService.saveUser(user);
+        if (user.getRole().getId()== SecurityConstants.ROLE_ADMIN)
+            model.addAttribute("successMessage", "Admin has been registered successfully! Please wait for admin approval");
+        else if (user.getRole().getId()== SecurityConstants.ROLE_BUYER)
+            model.addAttribute("successMessage", "Buyer has been registered successfully");
+        model.addAttribute("user", new User());
+        return "pages/security/registration";
+    }
+}
--- server/safeairlines/src/main/java/edu/mum/cs/cs452/safeairlines/utils/SecurityConstants.java
+++ server/safeairlines/src/main/java/edu/mum/cs/cs452/safeairlines/utils/SecurityConstants.java
@@ -2,5 +2,5 @@ package edu.mum.cs.cs452.safeairlines.utils;
 
 public class SecurityConstants {
     final public static long ROLE_ADMIN = 1;
-    final public static long ROLE_BUYER = 3;
+    final public static long ROLE_BUYER = 2;
 }
