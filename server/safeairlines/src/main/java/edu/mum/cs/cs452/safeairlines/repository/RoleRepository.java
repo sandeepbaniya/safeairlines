7440e0b7d2d3f0550ec6f503584a9e69cf85a591
iaidar <iaidar@mum.edu>
2019-10-16 08:10:54 -0500

    Add role repository

---
+++ server/safeairlines/src/main/java/edu/mum/cs/cs452/safeairlines/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs452.safeairlines.repository;
+
+import edu.mum.cs.cs452.safeairlines.model.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role,Long> {
+    Optional<Role> findById(Long id);
+}
